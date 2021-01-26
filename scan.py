013dab47ee4822a1128eb1c538d66fd33a5c5f9c
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-01-26 13:48:03 +0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -7,12 +7,17 @@ parser.add_argument('-p',"--programs", help="scan the programs Note: run as admi
                     action="store_true")
 parser.add_argument('-f',"--fast", help="fast scanner",
                     action="store_true")
+parser.add_argument('-ps',"--processing", help="scan processing on your device  don't forget to set your API key",
+                    action="store_true")
 args = parser.parse_args()
 if (args.connections):
     full()
+    ps()
 if(args.programs):
     programs()
 if(args.fast):
     fast()
+if(args.processing):
+    ps()
 else:
     print(f'python {sys.argv[0]} -h')
