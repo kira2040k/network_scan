9ab058579f26d8b60e5d5eea87c62808d9ad5541
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-01-26 13:47:07 +0300

    Update funs.py

--- funs.py
+++ funs.py
@@ -1,4 +1,4 @@
-import subprocess,re,socket,json,requests,argparse,time
+import subprocess,re,socket,json,requests,argparse,time,random,virus_total_apis,os
 def fast():
     output = subprocess.check_output("netstat -n", shell=True)
     output = output.decode("utf-8")
@@ -18,7 +18,7 @@ def fast():
             
 def full():
 
-    API = 'your api here'
+    API = 'your ip-geolocation.whoisxmlapi.com api'
     output = subprocess.check_output("netstat -n", shell=True)
     output = output.decode("utf-8")
     output2 = re.findall('[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}',output)
@@ -48,7 +48,7 @@ def full():
                 pass
 
 def programs():
-    print('please wait....')
+    print('scan running....')
     output = subprocess.check_output("netstat -nb", shell=True)
     time.sleep(8)
     output = output.decode("utf-8")
@@ -75,4 +75,40 @@ def programs():
                 
         except:
             pass
+
+def ps():
+    print('scan running....')
+    API = 'your virus total api'
+    API = virus_total_apis.PublicApi(API)
+    
+    os.system("powershell.exe \"get-process | get-item -erroraction silentlycontinue | format-table name, directory\" > proc.txt")
+    time.sleep(2)
+    file  = open('proc.txt','r')
+    Programs = file.read()
+    scan = re.findall('.*',Programs)
+    check = []
+    for pro in scan:
+        Programs_name  = re.findall('.*exe',pro)
+        
+        Progrmas_paths = re.findall('[A-Z]:.*',pro)
+        
+        try:
+            if('\\' in Progrmas_paths[0] and '.exe' in Programs_name[0] and Progrmas_paths[0] not in check):
+                new = re.sub(' +', ' ',Progrmas_paths[0])
+                new = new.rstrip()+'\\'+Programs_name[0]
+                output = subprocess.check_output(f"certutil -hashfile \"{new}\" MD5")
+                output = output.decode("utf-8")
+                
+                hash1 = re.findall('[a-z|A-Z|0-9]{32}',output)
+                print('\033[1;32;40m',new)
+                print(hash1[0])
+                check.append(str(Progrmas_paths[0]))
+                response  = API.get_file_report(hash1[0])
+                response = json.dumps(response)
+                response = json.loads(response)
+                print(f"{response['results']['positives']}/70")
+                time.sleep(26)
+                
+        except:
+            pass        
 
