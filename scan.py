f6f56f171280f4f33b248249c207508eadf6a089
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-01-30 14:51:54 +0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -13,6 +13,10 @@ parser.add_argument('-ps',"--processing", help="scan processing on your device  
 parser.add_argument('-b',"--banner", help="show tool banner",
                     action="store_true")
 parser.add_argument('-d',"--directory", help="scan files",action="store_true")
+parser.add_argument('-sf',"--ScanHide", help="scan for hidden files",
+                    action="store_true")
+parser.add_argument('-hf',"--hide", help="hide files",
+                    action="store_true")
 args = parser.parse_args()
 
 
@@ -27,5 +31,9 @@ if(args.processing):
     ps()
 if(args.directory):
     files()
+if(args.ScanHide):
+    Scanhide()
+if(args.hide):
+    hide()
 if(len(sys.argv) == 1):
     print(f'for help: python {sys.argv[0]} -h')
